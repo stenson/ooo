e294b91483bc49f066cb98a8fadd609cd396e3be
Rob Stenson <rob@johnmcneilstudio.com>
2011-04-13 10:49:55 -0700

    noodles romanov

--- ooo.js
+++ ooo.js
@@ -2,72 +2,53 @@ var _ = require("underscore")._;
 
 var ooo = (function() {
   // the functions state variables
-  var stack = [],
+  var saves = {},
+    stack = [],
     // the master of ceremonies
-    oooo = function(value) {
-      stack.push(value);
-      // helpers could be arbitrary length
-      if(value.is) {
-        stack.pop();
-        var helpers = _.toArray(arguments).slice(1),
-          args = stack.slice(-(value.is.length-helpers.length));
-        // add helpers to the end
-        args = args.concat(helpers);
-        var _value = value.is.apply(null,args);
+    oooo = function(value,depth) {
+      
+      if(value === oooo.unbox) return oooo.unbox();
+      // if it's a function, we should do something
+      else if(_.isFunction(value)) {
+        var helpers = _.toArray(arguments).slice(2),
+          args = stack.slice(-depth).concat(helpers),
+          _value = value.apply(null,args);
         stack.push(_value);
       }
-      else if(_.isFunction(value)) {
-        stack.pop();
-        value(stack.slice(-1)[0]);
-      }
+      // otherwise it's a value to throw on the stack
       else {
-        var args = _.toArray(arguments),
-          length = args.length;
-        // pushing a list-style array
-        if(length > 1) {
-          stack.pop();
-          stack.push(args);
-        }
+        var args = _.toArray(arguments);
+        stack.push(args);
       }
+      // always (except for unbox) the function itself
+      console.log("\n\n",saves,"\n\n",stack,"\n\n");
       return oooo;
     };
-  // give it back!
+  // add some standard control flow methods
+  _.extend(oooo,{
+    unbox: function(name) {
+      return (name) ? saves[name] : stack.slice(-1)[0];
+    },
+    save: function(value,name) {
+      saves[name] = value;
+    },
+    branch: function(a,b,condition) {
+      return condition ? a : b;
+    }
+  });
+  // now give it up!
   return oooo;
 })();
 
-// add control statements
-_.extend(ooo,{
-  branch: {
-    is: function(a,b,condition) {
-      return condition ? a : b;
-    }
-  },
-  map: {
-    is: function(xs,fn) {
-      _.map(xs,fn);
-    }
-  },
-  log: {
-    is: function(x) {
-      console.log(x);
-    }
-  }
-});
-
 with(ooo) {
   var x = 1;
-  // ooo it!
+  
   ooo
     (1, 2, 3)
     (4, 5, 6)
-    (branch, x === 3)
-    (log)
-    
-    // (map,function(n){
-    //   return n + 1;
-    // })
-    // (filter,function(n){
-    //   return n === 3;
-    // })
-    // (log)
+    (branch, 2, x === 3)
+    (_.map, 1, function(n){
+      return n + 1;
+    })
+    (save, 1, "x")
 }
